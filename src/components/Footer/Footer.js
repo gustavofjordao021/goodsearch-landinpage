943cea4c29f102f17b68be4a07569ccd973a436d
Gustavo Jordao <gustavo.jordao021@gmail.com>
2021-06-13 14:42:42 -0400

    Footer adjustments

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <>
       <footer className="shadow-md flex flex-row w-full text-xs px-4 py-4 mx-4 text-center bg-gray-50 justify-between md:px-12 xl:w-6/12">
-        <div className="mb-1">© 2021 Goodsearch</div>
+        <div className="mb-1">© 2021 GoodProtocol</div>
         <div className="justify-center text-center">
           <b>
             Made with{" "}
